053ee0de1d2378a808f07d415c5c29f4a514035b
David Browning <david.a.browning@gmail.com>
2025-03-17 11:44:10 +0100

    Initial build including setting up databases.

---
+++ PairProgrammingDatabasesCustomersAndOrders/Migrations/20250317103916_Update classes from using ids to using objects.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PairProgrammingDatabasesCustomersAndOrders.Migrations
+{
+    /// <inheritdoc />
+    public partial class Updateclassesfromusingidstousingobjects : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Descriptions",
+                table: "Products",
+                newName: "Description");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_OrderRows_ProductId",
+                table: "OrderRows",
+                column: "ProductId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_OrderRows_Products_ProductId",
+                table: "OrderRows",
+                column: "ProductId",
+                principalTable: "Products",
+                principalColumn: "ProductId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_OrderRows_Products_ProductId",
+                table: "OrderRows");
+
+            migrationBuilder.DropIndex(
+                name: "IX_OrderRows_ProductId",
+                table: "OrderRows");
+
+            migrationBuilder.RenameColumn(
+                name: "Description",
+                table: "Products",
+                newName: "Descriptions");
+        }
+    }
+}
---
+++ PairProgrammingDatabasesCustomersAndOrders/ProgramDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairProgrammingDatabasesCustomersAndOrders
+{
+    class ProgramDbContext : DbContext
+    {
+        private string connectionString = "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=PairProgrammingCustomersAndOrders;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<OrderRow> OrderRows { get; set; }
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+        }
+    }
+}
