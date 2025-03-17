685045dcc91ba88f1421af06eb87cd48834d0fc8
David Browning <david.a.browning@gmail.com>
2025-03-17 11:21:29 +0100

    Add project files.

---
+++ PairProgrammingDatabasesCustomersAndOrders/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace PairProgrammingDatabasesCustomersAndOrders
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
